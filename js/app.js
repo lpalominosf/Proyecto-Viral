988be163744e095083e383634f79af176aae6a9b
Lore Palominos <lorena.palominos@gmail.com>
2018-02-19 22:29:52 -0300

    añadiendo avance

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 // API KEY AIzaSyCieqwTyi9PwFlNRTb99Ok0AGVEw0cOaXE
+
+$(function() {
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    // prepare the request
+    var request = gapi.client.youtube.search.list({
+      part: "snippet",
+      type: "video",
+      q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+    });
+  });
+});
+
+function init() {
+  gapi.client.setApiKey("AIzaSyCieqwTyi9PwFlNRTb99Ok0AGVEw0cOaXE");
+  gapi.client.load("youtube", "v3", function() {
+    // yt api is ready
+  });
+}
